0ac8d88fe4b51351ee7760cd5ffe5cf3c1c8afcd
Ubuntu <ubuntu@ip-172-31-21-229.us-east-2.compute.internal>
2019-06-12 20:30:16 +0000

    Apt App change

---
+++ app/javascript/components/Apt.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Apt extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      
+      
+      </React.Fragment>
+    );
+  }
+}
+
+export default Apt
